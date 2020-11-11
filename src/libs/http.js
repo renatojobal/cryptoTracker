a7ca7d5d0a2a1b1c99e68d0342c698ddf6a3c771
Renato <renatojobal@gmail.com>
2020-11-11 18:22:37 -0500

    create wrapper Http
    Create wrapper Http for do get and post to the api about crypto

---
+++ src/libs/http.js
@@ -0,0 +1,39 @@
+class Http {
+    static instance = new Http();
+
+    get = async (url) => {
+        try {
+            let request = await fetch(url);
+            let json = await request.json();
+            return json;
+        } catch (error) {
+            console.log("Http GET method error ", error);
+            throw Error(error);
+        }
+    }
+
+
+    post = async (url, body) => {
+        try {
+            let req = await fetch(url,
+                {
+                    method: "POST",
+                    body
+                }
+
+            );
+
+            let json = await request.json();
+            return json;
+
+        } catch (error) {
+            console.log("Http POST method error ", error);
+            throw Error(error);
+        }
+    }
+
+
+
+}
+
+export default Http;

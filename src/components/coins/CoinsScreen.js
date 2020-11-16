39db867f2f860aaa01fa7f39effd165eafaf712b
Renato <renatojobal@gmail.com>
2020-11-15 21:08:54 -0500

    feat: create text input for search

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -9,25 +9,45 @@ import {
 } from 'react-native';
 import Http from 'cryptoTracker/src/libs/http';
 import CoinsItem from './CoinsItem';
+import CoinsSeacrh from './CoinsSearch';
 import Colors from 'cryptoTracker/src/res/colors';
 
 class CoinsScreen extends Component {
   state = {
     coins: [],
+    allCoins: [],
     loading: false,
   };
 
-  componentDidMount = async () => {
+  componentDidMount = () => {
+    this.getCoins();
+  };
+
+  getCoins = async () => {
     this.setState({loading: true});
+
     const res = await Http.instance.get(
       'https://api.coinlore.net/api/tickers/',
     );
 
-    this.setState({coins: res.data, loading: false});
+    this.setState({coins: res.data, allCoins: res.data, loading: false});
   };
 
   handlePress = (coin) => {
-    this.props.navigation.navigate('CoinDetail', { coin });
+    this.props.navigation.navigate('CoinDetail', {coin});
+  };
+
+  handleSearch = (query) => {
+    const {allCoins} = this.state;
+
+    const coinsFiltered = allCoins.filter((coin) => {
+      return (
+        coin.name.toLowerCase().includes(query.toLowerCase()) ||
+        coin.symbol.toLowerCase().includes(query.toLowerCase())
+      );
+    });
+
+    this.setState({coins: coinsFiltered});
   };
 
   render() {
@@ -35,6 +55,8 @@ class CoinsScreen extends Component {
 
     return (
       <View style={styles.container}>
+        <CoinsSeacrh onChange={this.handleSearch} />
+
         {loading ? (
           <ActivityIndicator style={styles.loader} color="#fff" size="large" />
         ) : null}
